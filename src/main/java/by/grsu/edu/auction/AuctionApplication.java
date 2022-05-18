dd6ee78e6d3306f6734ccd2ca99d5078f9d9706f
SofiKj17 <103689891+sofikj17@users.noreply.github.com>
2022-05-18 21:55:12 +0300

    Created maven project. Added dependencies.
    Auction 1 Create project and structure

---
+++ src/main/java/by/grsu/edu/auction/AuctionApplication.java
@@ -0,0 +1,16 @@
+package by.grsu.edu.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = {"by.grsu.edu.auction"})
+@EntityScan(basePackages = {"by.grsu.edu.auction.entity"})
+//@EnableJpaRepositories(basePackages = {"by.vodmvi.dms.documents.repositories"})
+public class AuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApplication.class, args);
+	}
+
+}
